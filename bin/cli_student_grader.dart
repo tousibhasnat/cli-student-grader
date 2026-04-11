d64c854e77525a309e7d5f7e2129e56a38446028
Tousib Hasnat <111671002+tousibhasnat@users.noreply.github.com>
2026-04-11 18:21:07 +0600

    Update cli_student_grader.dart

--- bin/cli_student_grader.dart
+++ bin/cli_student_grader.dart
@@ -1,5 +1,4 @@
-import 'package:cli_student_grader/cli_student_grader.dart' as cli_student_grader;
-
-void main(List<String> arguments) {
-  print('Hello world: ${cli_student_grader.calculate()}!');
+void maim(){
+  const appName  = 'appName Student Grader v1.0';
+  print (appName);
 }
