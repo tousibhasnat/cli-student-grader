37644e096304edae23e25e8757d731c2b78db716
Tousib Hasnat <111671002+tousibhasnat@users.noreply.github.com>
2026-04-11 18:10:53 +0600

    feat: initial project scaffold

---
+++ bin/cli_student_grader.dart
@@ -0,0 +1,5 @@
+import 'package:cli_student_grader/cli_student_grader.dart' as cli_student_grader;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cli_student_grader.calculate()}!');
+}
